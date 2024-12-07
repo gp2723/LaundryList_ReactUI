08e41da83bb6b48d9d41764c5cf64421c55c50a0
Maximo Librandi <maximolibrandi@hotmail.com>
2024-12-07 18:23:47 -0500

    Add AI microservice handler

--- src/components/Assignments.js
+++ src/components/Assignments.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+
+import { postGenerateAIResponse } from '../services/api';
 
 // Assignment Details Component
 const AssignmentDetails = () => {
     const { courseId } = useParams(); // Get courseId from the URL
     const [assignment, setAssignment] = useState(null);
     const [courseName, setCourseName] = useState('');
-  
+
+    const [show, setShow] = useState(false);
+    const [userInput, setUserInput] = useState('');
+    const [generatedAIResponse, setGeneratedAIResponse] = useState('');
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
     useEffect(() => {
       // Dummy data for courses and assignments
       const dummyCourses = [
@@ -18,10 +30,10 @@ const AssignmentDetails = () => {
   
       // Dummy assignment data
       const assignmentData = {
-        1: { dueDate: '2024-12-15', isQuiz: 'No', dateCreated: '2024-11-01' },
-        2: { dueDate: '2024-11-30', isQuiz: 'Yes', dateCreated: '2024-10-25' },
-        3: { dueDate: '2024-11-05', isQuiz: 'No', dateCreated: '2024-10-01' },
-        4: { dueDate: '2024-10-15', isQuiz: 'Yes', dateCreated: '2024-09-15' },
+        1: { title: 'Assignment 1', dueDate: '2024-12-15', isQuiz: 'No', dateCreated: '2024-11-01', description: 'This assignment is about Cloud Computing' },
+        2: { title: 'Assignment 2', dueDate: '2024-11-30', isQuiz: 'Yes', dateCreated: '2024-10-25', description: 'This assignment is about Data Science' },
+        3: { title: 'Assignment 3', dueDate: '2024-11-05', isQuiz: 'No', dateCreated: '2024-10-01', description: 'This assignment is about Web Development' },
+        4: { title: 'Assignment 4', dueDate: '2024-10-15', isQuiz: 'Yes', dateCreated: '2024-09-15', description: 'This assignment is about Machine Learning' },
       };
   
       // Find the course name based on courseId
@@ -34,10 +46,24 @@ const AssignmentDetails = () => {
       const assignmentDetails = assignmentData[courseId];
       setAssignment(assignmentDetails);
     }, [courseId]);
+
+    // Function to generate AI response
+    const handleGenerateAIResponse = () => {
+        
+        // Wait for the AI response
+        const response = postGenerateAIResponse(userInput, assignment.title, assignment.description);
+
+        // Show the response
+        console.log('AI response generated');
+        console.log(response);
+        setGeneratedAIResponse(response);
+    };
+
   
     return (
       <div>
         {assignment ? (
+          <>
           <div>
             <h2>Assignment Details</h2>
             <br></br>
@@ -46,6 +72,40 @@ const AssignmentDetails = () => {
             <p><strong>Due Date:</strong> {assignment.dueDate}</p>
             <p><strong>Is Quiz:</strong> {assignment.isQuiz}</p>
           </div>
+
+          <Button
+            color="primary"
+            onClick={handleShow}
+          >
+            Ask SurfAI
+          </Button>
+
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>SurfAI</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label>Get help from SurfAI to generate a response for this assignment.</Form.Label>
+                  <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+              </Form>
+              <Button color="primary" onClick={handleGenerateAIResponse} >
+                Ask
+              </Button>
+              <br />
+              <br />
+              <p>AI response will be displayed here...</p>
+            </Modal.Body> 
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        
+          </>
         ) : (
           <p>Loading...</p>
         )}
--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,18 @@
 // This API service will make the HTTP requests to the backend APIs.
+import axios from 'axios';
+
+// ## GenAI API requests ##
+
+// POST method to send user input, assigment title, and description
+export const postGenerateAIResponse = async (userInput, assignmentTitle, description) => {
+    try {
+        const response = await
+         axios.post('http://44.202.47.119:8000/generate_AI_response', {
+            user_message: userInput,
+            assignment_title_description: assignmentTitle + '\n\n' + description
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error while fetching data from GenAI API', error);
+    }
+}
